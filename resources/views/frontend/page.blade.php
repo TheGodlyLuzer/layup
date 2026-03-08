0f70faab091b6f34bed09cc4f345b3bf8dd5809f
Chase Miller <chase@crumbls.com>
2026-03-07 19:32:03 -0700

    Add frontend loop for reusability.

--- resources/views/frontend/page.blade.php
+++ resources/views/frontend/page.blade.php
@@ -24,10 +24,5 @@
     </x-slot:meta>
     @endif
 
-    <div @if($page->id) data-page-id="{{ $page->id }}" @endif>
-        @foreach($sections as $section)
-            @include('layup::components.section', ['section' => $section])
-        @endforeach
-    </div>
-    @layupScripts
+    @include('layup::frontend.loop')
 </x-dynamic-component>
